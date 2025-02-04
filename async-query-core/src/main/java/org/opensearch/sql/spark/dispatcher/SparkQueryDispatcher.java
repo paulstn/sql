0f20de4a53e48d7756774e0adf28485eccafe586
Paul Sebastian <paulstn@amazon.com>
2025-02-04 13:28:08 -0800

    using async query api to accept promql query req w/ prom datasource
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>

--- async-query-core/src/main/java/org/opensearch/sql/spark/dispatcher/SparkQueryDispatcher.java
+++ async-query-core/src/main/java/org/opensearch/sql/spark/dispatcher/SparkQueryDispatcher.java
@@ -5,15 +5,29 @@
 
 package org.opensearch.sql.spark.dispatcher;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
-import org.jetbrains.annotations.NotNull;
+import lombok.NonNull;
+import okhttp3.OkHttpClient;
 import org.json.JSONObject;
+import org.opensearch.sql.common.interceptors.URIValidatorInterceptor;
 import org.opensearch.sql.datasource.DataSourceService;
 import org.opensearch.sql.datasource.model.DataSourceMetadata;
+import org.opensearch.sql.datasource.model.DataSourceType;
 import org.opensearch.sql.spark.asyncquery.model.AsyncQueryJobMetadata;
 import org.opensearch.sql.spark.asyncquery.model.AsyncQueryRequestContext;
+import org.opensearch.sql.spark.asyncquery.model.QueryState;
+import org.opensearch.sql.spark.client.PrometheusClient;
+import org.opensearch.sql.spark.client.PrometheusClientImpl;
 import org.opensearch.sql.spark.dispatcher.model.DispatchQueryContext;
 import org.opensearch.sql.spark.dispatcher.model.DispatchQueryRequest;
 import org.opensearch.sql.spark.dispatcher.model.DispatchQueryResponse;
@@ -42,12 +56,130 @@ public class SparkQueryDispatcher {
   private final SQLQueryValidator sqlQueryValidator;
   private final PPLQueryValidator pplQueryValidator;
 
+  // private final PrometheusClient prometheusClient;
+
   public DispatchQueryResponse dispatch(
       DispatchQueryRequest dispatchQueryRequest,
       AsyncQueryRequestContext asyncQueryRequestContext) {
     DataSourceMetadata dataSourceMetadata =
         this.dataSourceService.verifyDataSourceAccessAndGetRawMetadata(
             dispatchQueryRequest.getDatasource(), asyncQueryRequestContext);
+
+    System.out.println("data source name: " + dataSourceMetadata.getName());
+    System.out.println(
+        "prom uri: "
+            + dataSourceMetadata.getProperties().getOrDefault("prometheus.uri", "no prom uri"));
+    if (LangType.PROMQL.equals(dispatchQueryRequest.getLangType())
+        && dataSourceMetadata.getConnector() == DataSourceType.PROMETHEUS) {
+
+      try {
+        PrometheusClient prometheus =
+            AccessController.doPrivileged(
+                (PrivilegedExceptionAction<PrometheusClientImpl>)
+                    () -> {
+                      try {
+                        return new PrometheusClientImpl(
+                            getHttpClient(),
+                            new URI(
+                                dataSourceMetadata
+                                    .getProperties()
+                                    .getOrDefault("prometheus.uri", "no prom uri")));
+                      } catch (URISyntaxException e) {
+                        throw new IllegalArgumentException(
+                            String.format(
+                                "Invalid URI in prometheus properties: %s", e.getMessage()));
+                      }
+                    });
+
+        // new PrometheusClientImpl(
+        //     getHttpClient(),
+        //     new URI(
+        //         dataSourceMetadata
+        //             .getProperties()
+        //             .getOrDefault("prometheus.uri", "no prom uri")));
+
+        System.out.println("created prom client impl");
+        JSONObject res =
+            AccessController.doPrivileged(
+                (PrivilegedExceptionAction<JSONObject>)
+                    () -> {
+                      try {
+                        return prometheus.queryRange(
+                            dispatchQueryRequest.getQuery(), 1737964800L, 1738137600L, "1h");
+                      } catch (IOException e) {
+                        e.printStackTrace();
+                        return new JSONObject();
+                      }
+                    });
+
+        // System.out.println("res: " + res);
+
+        return DispatchQueryResponse.builder()
+            .queryId(res.toString())
+            .jobId("yourJobId")
+            .resultIndex("yourResultIndex")
+            .sessionId("yourSessionId")
+            .datasourceName("yourDatasourceName")
+            .jobType(JobType.INTERACTIVE)
+            .indexName("yourIndexName")
+            .status(QueryState.SUCCESS)
+            .build();
+
+      } catch (PrivilegedActionException e) {
+        e.printStackTrace();
+        return DispatchQueryResponse.builder()
+            .queryId("yourQueryId")
+            .jobId("yourJobId")
+            .resultIndex("yourResultIndex")
+            .sessionId("yourSessionId")
+            .datasourceName("yourDatasourceName")
+            .jobType(JobType.INTERACTIVE)
+            .indexName("yourIndexName")
+            .status(QueryState.FAILED)
+            .build();
+      }
+
+      // DataSource prometheus = this.dataSourceService.getDataSource(dataSourceMetadata.getName());
+
+      // String prometheusUri =
+      //     dataSourceMetadata.getProperties().getOrDefault("prometheus.uri", "no prom uri");
+      // // make a call to prometheus
+
+      // Collection<FunctionResolver> functionList = (prometheus.getStorageEngine()).getFunctions();
+      // Optional<FunctionResolver> queryRangeResolver =
+      //     functionList.stream()
+      //         .filter(function -> function.getFunctionName().toString().equals("query_range"))
+      //         .findFirst();
+
+      // if (queryRangeResolver.isPresent()) {
+      //   Pair<FunctionSignature, FunctionBuilder> queryRangeResolved =
+      //       queryRangeResolver.get().resolve(null);
+      //   List<Expression> arguments = new ArrayList<Expression>();
+      //   arguments.add(
+      //       new NamedArgumentExpression(
+      //           "query",
+      //           new LiteralExpression(
+      //               new ExprStringValue("prometheus_tsdb_head_series{job=\"prometheus\"}"))));
+      //   arguments.add(
+      //       new NamedArgumentExpression(
+      //           "starttime", new LiteralExpression(new ExprLongValue(1737964800))));
+      //   arguments.add(
+      //       new NamedArgumentExpression(
+      //           "endtime", new LiteralExpression(new ExprLongValue(1738137600))));
+      //   arguments.add(
+      //       new NamedArgumentExpression("step", new LiteralExpression(new
+      // ExprStringValue("1h"))));
+
+      //   Table something =
+      //       ((TableFunctionImplementation)
+      //               queryRangeResolved.getRight().apply((FunctionProperties) null, arguments))
+      //           .applyArguments();
+
+      //   System.out.println("table: " + something);
+      // } else {
+      //   System.out.println("query_range function is not present");
+      // }
+    }
 
     String query = dispatchQueryRequest.getQuery();
     if (LangType.SQL.equals(dispatchQueryRequest.getLangType())) {
@@ -126,14 +258,14 @@ public class SparkQueryDispatcher {
     }
   }
 
-  @NotNull
+  @NonNull
   private AsyncQueryHandler getDefaultAsyncQueryHandler(String accountId) {
     return sessionManager.isEnabled()
         ? queryHandlerFactory.getInteractiveQueryHandler()
         : queryHandlerFactory.getBatchQueryHandler(accountId);
   }
 
-  @NotNull
+  @NonNull
   private static IndexQueryDetails getIndexQueryDetails(DispatchQueryRequest dispatchQueryRequest) {
     IndexQueryDetails indexQueryDetails =
         SQLQueryUtils.extractIndexDetails(dispatchQueryRequest.getQuery());
@@ -209,5 +341,14 @@ public class SparkQueryDispatcher {
     tags.put(CLUSTER_NAME_TAG_KEY, dispatchQueryRequest.getClusterName());
     tags.put(DATASOURCE_TAG_KEY, dispatchQueryRequest.getDatasource());
     return tags;
+  }
+
+  private OkHttpClient getHttpClient() {
+    OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
+    okHttpClient.callTimeout(1, TimeUnit.MINUTES);
+    okHttpClient.connectTimeout(30, TimeUnit.SECONDS);
+    okHttpClient.followRedirects(false);
+    okHttpClient.addInterceptor(new URIValidatorInterceptor(new ArrayList<>()));
+    return okHttpClient.build();
   }
 }
